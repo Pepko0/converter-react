3d42cd523f5941183cfdc987af90e25607d93692
Kacper Lewko <k.lewkosga@gmail.com>
2023-06-23 18:19:50 +0200

    added style-components to Result

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,23 +1,23 @@
-import "./style.css"
+import {Box, Title, Numbers} from "./styled.js";
 
 
 const Result = ({ result }) => {
 
     return (
-        <p className="result">
-            <label className="result__box">
-                <span className="result__title">Otrzymasz:</span>
-                <span className="result__number">
+        <p>
+            <Box>
+                <Title>Otrzymasz:</Title>
+                <Numbers>
                     {result && (
                         <>
-                            {result.sourceAmount.toFixed(2)} PLN =
+                            {result.sourceAmount.toFixed(2)} PLN = 
 
-                            {result.targetAmount.toFixed(2)}{result.currency}
+                            {result.targetAmount.toFixed(2)}{ result.currency}
 
                         </>
                     )}
-                </span>
-            </label>
+                </Numbers>
+            </Box>
         </p>
     )
 }
---
+++ src/Form/Result/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Box = styled.label`
+    display: grid;
+    grid-template-columns: auto;
+`;
+
+export const Title = styled.span`
+    font-weight: bold;
+    font-size: 18px;
+    margin: auto;
+`;
+
+export const Numbers = styled.span`
+    font-size: 25px;
+    padding: 10px;
+    margin: 0 auto;
+    font-weight: bold;
+`;
